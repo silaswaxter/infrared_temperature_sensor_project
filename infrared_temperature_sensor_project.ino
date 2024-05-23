eaba39b68bb334ef73a234a0e9cd5343737116b2
Silas Waxter <waxters@oregonstate.edu>
2024-05-23 11:57:22 -0700

    feat: stream voltage on pin 2 to serial monitor

--- infrared_temperature_sensor_project.ino
+++ infrared_temperature_sensor_project.ino
@@ -1,9 +1,15 @@
-void setup() {
-  // put your setup code here, to run once:
+#define ANALOG_READ_PIN 2
 
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
 }
 
+// the loop routine runs over and over again forever:
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  auto analog_value = analogRead(ANALOG_READ_PIN);
+  double voltage = (analog_value / 4095) * 3.3;
+  Serial.println(voltage);
 }
